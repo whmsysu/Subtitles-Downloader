528b45d9fe0d810c0d7be04060df4096cb2501a8
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-18 15:31:11 -0400

    Fix bootstrap require jquery error

--- list-operation.js
+++ list-operation.js
@@ -1,7 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-window.$ = window.jQuery = require('jquery');
 $(".list-group-item").click(function(e){
   if ($(this).hasClass("active")) {
     $(this).removeClass("active");
