451d435e02806d22c05d3bd660bd77b819caee33
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-23 15:35:07 -0400

    Bug fix

--- shooter-download-helper.js
+++ shooter-download-helper.js
@@ -2,17 +2,17 @@ exports.downloadShooterSub = (li, videoFilePath) =>{
     var Fn = require('shooter').API.fetch;
     var path = require('path');
     Fn(videoFilePath, function(err, res) {
-      if (!err) {
-        $(li).css("text-decoration", "line-through");
-        $(li).attr("is-downloaded", 1);
         window.total_download_files--;
         if (window.total_download_files === 0) {
             $("#download-button").text("Donwload All");
             $("#download-button").prop('disabled', false);
         }
-        console.log(path.basename(videoFilePath), '->', res);
-      } else {
-        console.log(err);
-      }
+        if (!err) {
+            $(li).css("text-decoration", "line-through");
+            $(li).attr("is-downloaded", 1);
+            console.log(path.basename(videoFilePath), '->', res);
+        } else {
+            console.log(err);
+        }
     });
 };
