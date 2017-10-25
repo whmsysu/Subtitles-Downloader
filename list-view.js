0f55f12c94abae0d679be60afef3191936253275
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-25 14:17:52 -0400

    1.Code style change 2. Event proxy

--- list-view.js
+++ list-view.js
@@ -1,8 +1,8 @@
 var fun = require('./import/collections/functions.js');
 const { downloadShooterSub } = require('./shooter-download-helper.js');
 
-
-window.total_download_files = 0;
+var EventProxy = require('eventproxy');
+window.ep = new EventProxy();
 
 $("#media-files-list").click(function(e){
   const now_item = e.target;
@@ -24,51 +24,50 @@ $("#remove-button").click(function(e){
 $("#download-button").click(function(e){
   $(this).text("Searching ...");
   $(this).prop('disabled', true);
-
   let listItems = $(".list-group-item");
-  total_download_files = listItems.length;
+
+  window.ep.after('api_callback', listItems.length, function (list) {
+    $("#download-button").text("Donwload All");
+    $("#download-button").prop('disabled', false);
+  });
+
   listItems.each(function(idx, li) {
       if ($(li).attr('is-downloaded') === "1") {
-        window.total_download_files--;
-        if (window.total_download_files === 0) {
-          $("#download-button").text("Donwload All");
-          $("#download-button").prop('disabled', false);
-        }
+        window.ep.emit('api_callback', {});
         return;
       }
       const videoFilePath = $(li).attr('absolute-path');
       downloadShooterSub(li, videoFilePath);
   });
-
   // const { downloadFile } = require('./download-helper.js');
   // downloadFile('http://www.pdf995.com/samples/pdf.pdf', '/Users/haominwu/Downloads/save.pdf');
 });
 
 var fileUploader = document.getElementById('media-files-list')      
-		window.ondragover = function(e) {
-			e.preventDefault();
-			return false
-		};
-    
-    window.ondragend = function(e) {
-			e.preventDefault();
-			return false
-		};
-    
-    window.ondrop = function(e) {
-			e.preventDefault();
-			return false
-		};
-    
-    fileUploader.addEventListener('dragover', function(e) {
-			e.preventDefault();
-			this.classList.add('hover');
-			e.dataTransfer.dropEffect = 'copy';
-    }, false);
-    
-		fileUploader.addEventListener('dragleave', function(e) {
-			e.preventDefault();
-			this.classList.remove('hover');
-    }, false);
-    
-    fileUploader.addEventListener('drop', fun.dropFuc, false);
+window.ondragover = function(e) {
+  e.preventDefault();
+  return false
+};
+
+window.ondragend = function(e) {
+  e.preventDefault();
+  return false
+};
+
+window.ondrop = function(e) {
+  e.preventDefault();
+  return false
+};
+
+fileUploader.addEventListener('dragover', function(e) {
+  e.preventDefault();
+  this.classList.add('hover');
+  e.dataTransfer.dropEffect = 'copy';
+}, false);
+
+fileUploader.addEventListener('dragleave', function(e) {
+  e.preventDefault();
+  this.classList.remove('hover');
+}, false);
+
+fileUploader.addEventListener('drop', fun.dropFuc, false);
--- shooter-download-helper.js
+++ shooter-download-helper.js
@@ -2,11 +2,7 @@ exports.downloadShooterSub = (li, videoFilePath) =>{
     var Fn = require('shooter').API.fetch;
     var path = require('path');
     Fn(videoFilePath, function(err, res) {
-        window.total_download_files--;
-        if (window.total_download_files === 0) {
-            $("#download-button").text("Donwload All");
-            $("#download-button").prop('disabled', false);
-        }
+        window.ep.emit('api_callback', {});
         if (!err) {
             $(li).css("text-decoration", "line-through");
             $(li).attr("is-downloaded", 1);
