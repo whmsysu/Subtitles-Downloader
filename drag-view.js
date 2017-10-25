d8027a64eadeef8bba49e1ca8d3d931337c9acce
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-25 17:48:41 -0400

    Style Change

--- drag-view.js
+++ drag-view.js
@@ -2,30 +2,30 @@ const fun = require('./import/collections/functions.js');
 
 let fileUploader = document.getElementById('drag-view');
 
-window.ondragover = function(e) {
-  e.preventDefault();
-  return false
+window.ondragover = function (e) {
+    e.preventDefault();
+    return false
 };
 
-window.ondragend = function(e) {
-  e.preventDefault();
-  return false
+window.ondragend = function (e) {
+    e.preventDefault();
+    return false
 };
 
-window.ondrop = function(e) {
-  e.preventDefault();
-  return false
+window.ondrop = function (e) {
+    e.preventDefault();
+    return false
 };
 
-fileUploader.addEventListener('dragover', function(e) {
-	e.preventDefault();
-	this.classList.add('hover');
-	e.dataTransfer.dropEffect = 'copy';
+fileUploader.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    this.classList.add('hover');
+    e.dataTransfer.dropEffect = 'copy';
 }, false);
 
-fileUploader.addEventListener('dragleave', function(e) {
-	e.preventDefault();
-	this.classList.remove('hover');
+fileUploader.addEventListener('dragleave', function (e) {
+    e.preventDefault();
+    this.classList.remove('hover');
 }, false);
 
 fileUploader.addEventListener('drop', fun.dropFuc, false);
