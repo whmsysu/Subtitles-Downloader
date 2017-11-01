73c99abf7a0be14f62921383172c2f28e1785f85
Haomin Wu <wuhaominsysu@gmail.com>
2017-11-01 14:56:28 -0400

    React init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: [
+      './view.js'
+    ],
+    output: {
+      path: __dirname,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [{
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+      },
+      {
+      }
+      ]
+    },
+  };
+  
